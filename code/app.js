31a3756964e5c14b77c39b8b258d38846c2b7295
Chris Aaron Luiz <luizchris23@gmail.com>
2023-06-30 18:04:16 +0530

    Update app.js

---
+++ code/app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const accesslog = require('access-log');
+const hostname = '0.0.0.0';
+const port = 3000;
+ 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  accesslog(req, res);
+  res.end('<h1><center>Hello World! this is a simple nodejs app running in a Docker container</h1></center>');
+});
+ 
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
